9a2f11ab5fbcdb23e7cb22077bab633cb0df3b17
Shudhanshu Ranjan <62972853+shudhanshu03@users.noreply.github.com>
2022-03-15 19:42:13 +0530

    Time: 24 ms (78.01%), Space: 11.1 MB (66.20%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int n=s.length();
+        // Step 1 : Iterate from start
+        int count=0; 
+        for(int i=0;i<n;++i){
+            if(s[i]=='('){ // for open bracket
+                ++count;
+            }
+            else if(s[i]==')'){ // for close bracket
+                if(count==0){  // if no. of close brackets > no. of open brackets
+                    s[i]='#';
+                }
+                else{
+                    // if matching parentheses found decrease count
+                    --count;
+                }
+            }
+        } 
+             count=0;
+        for(int i=n-1;i>=0;--i){
+            if(s[i]==')'){ // for close bracket
+                ++count;
+            }
+            else if(s[i]=='('){ // for open bracket
+                if(count==0){ // if no. of open brackets > no. of close brackets
+                    s[i]='#';
+                }
+                else{
+                    // if matching parentheses found decrease count
+                    --count;
+                }
+            }
+        }
+            string ans="";
+        for(int i=0;i<n;++i){
+            if(s[i]!='#'){ 
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    
+    }
+};
