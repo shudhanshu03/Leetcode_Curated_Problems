38752bb48671c9ba6d9abe85823986557391491a
Shudhanshu Ranjan <62972853+shudhanshu03@users.noreply.github.com>
2022-04-03 03:14:39 +0530

    Time: 14 ms (30.47%), Space: 10.6 MB (67.98%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -7,14 +7,17 @@ public:
             res.push_back(temp);
             return;
         }
+       if(target < 0)
+       {
+return;
+           }
         if(i>=candidates.size())
         {
             return;
         }
         
         
-        if(target>=candidates[i])
-        {
+        
           temp.push_back(candidates[i]);
         
           solve(i+1,temp,res,candidates,target-candidates[i]);
@@ -27,7 +30,7 @@ public:
             }
            solve(i+1,temp,res,candidates,target);
             
-        }
+          
   }
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
