68b7fdbcd1dd2a0d7097a794abe05276359c1dc6
Shudhanshu Ranjan <62972853+shudhanshu03@users.noreply.github.com>
2022-04-14 18:11:44 +0530

    Time: 4 ms (23.16%), Space: 6.6 MB (59.40%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    vector<string>map = {"abc", "def", "ghi" , "jkl" , "mno" , "pqrs", "tuv" , "wxyz"};
-        
+    vector<string>dir = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
-    void helper(int ii, string &s, vector<string>&res , string digits)
+    void dfs(int ii,vector<string>&res,string &ans , string &digits)
     {
         if(ii==digits.size())
         {
-            res.push_back(s);
+            res.push_back(ans);
             return;
         }
-        string dum = map[digits[ii]-'2'];
+        
+        string dum = dir[digits[ii]-'2'];
         for(int i=0;i<dum.size();i++)
         {
-            s.push_back(dum[i]);
-            helper(ii+1,s,res,digits);
-            s.pop_back();
+            ans.push_back(dum[i]);
+            dfs(ii+1,res,ans,digits);
+            ans.pop_back();
         }
+        
     }
+    
     vector<string> letterCombinations(string digits) {
+        vector<string>res;
         if(digits=="")
         {
             return {};
         }
-        vector<string>res;
-        string s="";
-        helper(0,s,res,digits);
-        
+        string ans="";
+        dfs(0,res,ans,digits);
         return res;
+        
     }
 };
