97a628327339bcd85796a2f75682f473c4687385
Shudhanshu Ranjan <62972853+shudhanshu03@users.noreply.github.com>
2022-04-15 16:30:12 +0530

    Time: 178 ms (86.05%), Space: 60.8 MB (66.19%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>id(n+1,0);
-        vector<int>od(n+1,0);
+        vector<int>o(n,0);
+        vector<int>ii(n,0);
         
         for(int i=0;i<trust.size();i++)
         {
-            od[trust[i][0]]++;
-            id[trust[i][1]]++;
+            o[trust[i][0]-1]++;
+            ii[trust[i][1]-1]++;
         }
         
-        for(int i=1;i<=n;i++){
-            if(od[i]==0 && id[i]==n-1)
+        for(int i=0;i<n;i++)
+        {
+            if(ii[i]==n-1 && o[i]==0)
             {
-                return i;
+                return i+1;
             }
         }
-       return -1; 
+        return -1;
     }
 };
