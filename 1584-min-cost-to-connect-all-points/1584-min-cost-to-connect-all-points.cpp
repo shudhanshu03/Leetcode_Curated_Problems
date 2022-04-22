40b796d43c07e9f84832fe54d8eb78bb9ab3d490
Shudhanshu Ranjan <62972853+shudhanshu03@users.noreply.github.com>
2022-04-22 14:22:16 +0530

    Time: 2420 ms (5.07%), Space: 246.5 MB (5.06%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    int find(int a , vector<int>&parent)
+    {
+        if(parent[a]==a)
+        {
+            return a;
+        }
+        return parent[a] = find(parent[a],parent);
+    }
+    
+    void unionn(int a , int b , vector<int>&rank , vector<int>&parent)
+    {
+        int aa = find(a,parent);
+        int bb = find(b,parent);
+        
+        if(rank[aa]<rank[bb])
+        {
+            parent[aa]=bb;
+        }
+        else if(rank[bb]<rank[aa])
+        {
+            parent[bb]=aa;
+        }
+        else{
+            parent[aa]=bb;
+            rank[bb]++;
+        }
+    }
+    
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        
+        vector<vector<int>>graph;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int res = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
+                graph.push_back({res,i,j});    
+            }
+        }
+        sort(graph.begin(),graph.end());
+        vector<int>parent(n);
+        vector<int>rank(n,0);
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+        int res = 0;
+        for(auto it:graph)
+        {
+            if(find(it[1],parent)!=find(it[2],parent))
+            {
+                res+=it[0];
+                unionn(it[1],it[2],rank,parent);
+            }
+        }
+        return res;
+        
+        
+        
+    }
+};
