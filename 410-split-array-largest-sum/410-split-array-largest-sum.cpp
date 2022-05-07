8b4b10d94eacc23a9f4ecc32a7fd2e31692cdf20
Shudhanshu Ranjan <62972853+shudhanshu03@users.noreply.github.com>
2022-05-07 17:56:23 +0530

    Time: 2 ms (69.49%), Space: 7.1 MB (95.30%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,22 +1,49 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int m) {
-        int l=0,r=0,n=nums.size();
-        for(int i=0;i<n;++i) l=max(l,nums[i]), r+=nums[i];
+    bool isValid(vector<int>&nums , int mid , int m)
+    {
+        int s = 0;
+        int c = 1;
         
-        int mid=0,ans=0;
-        while(l<=r){
-            mid=(l+r)/2;
-            int count=0,tempsum=0;
-            for(int i=0;i<n;++i){
-                if(tempsum+nums[i]<=mid) tempsum+=nums[i];
-                else count++,tempsum=nums[i];
+        for(auto i:nums)
+        {
+            s+=i;
+            if(s>mid)
+            {
+                s=i;
+                c++;
             }
-            count++; 
+            if(c>m)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    int splitArray(vector<int>& nums, int m) {
+        int l = INT_MIN;
+        int h = 0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            l = max(l,nums[i]);
+            h+=nums[i];
+        }
+        int res;
+        while(l<=h)
+        {
+            int mid = l+(h-l)/2;
             
-            if(count<=m) r=mid-1, ans=mid;
-            else l=mid+1;
-        }  
-        return ans;
+            if(isValid(nums,mid,m))
+            {
+                res = mid;
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return res;
     }
 };
