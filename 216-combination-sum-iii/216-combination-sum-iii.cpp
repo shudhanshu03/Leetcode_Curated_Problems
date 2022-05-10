327bb9621d91a58412e28be717dc8c646cf8c700
Shudhanshu Ranjan <62972853+shudhanshu03@users.noreply.github.com>
2022-05-10 19:48:09 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (94.37%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -4,27 +4,27 @@ public:
     {
         if(n<0)
         {
-            return;
+            return; // Return ; No need to check further as the required sum till now become greater than n 
         }
         if(temp.size()==k && n==0)
         {
-            res.push_back(temp);
+            res.push_back(temp); // Found the combination so just push that into res vector
             return;
         }
         
-        for(int i = start; i<=9;i++)
+        for(int i=start;i<=9;i++)
         {
-            temp.push_back(i);
-            helper(res,temp,i+1,n-i,k);
-            temp.pop_back();
+            temp.push_back(i); // includes the ith value
+            helper(res,temp,i+1,n-i,k); // check values after that as each number should be used at most once
+            temp.pop_back(); // pop_back the last value included as that combination doesn't make the sum
         }
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>>res;
-        vector<int>temp;
-        helper(res,temp,1,n,k);
+        vector<vector<int>>res; // creating the resultant vector
+        vector<int>temp; // creating temp vector to store possible combinations
+        helper(res,temp,1,n,k); // calling the helper function
         
-        return res;
+        return res; // returning our resultant vector
     }
 };
